ff2fc8a6e31aaa4778afd1772b1478f2b868b53d
Marcos Novaes <marcosnovaes@id.uff.br>
2020-09-25 11:33:21 -0300

    trocando base url pra url do heroku

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { getToken } from './auth'
 
 const clientHTTP = axios.create({
-    baseURL: `http://localhost:3001`
+    baseURL: `https://bootcamp-app-receitas.herokuapp.com`
 })
 
 clientHTTP.defaults.headers.post['Content-Type'] = 'application/json';
